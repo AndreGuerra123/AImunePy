633d455fdab93eb650fec1936db470e3d358811d
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-07 21:06:46 +0100

    importing keras and pymongo

--- controllers/train.py
+++ controllers/train.py
@@ -1,2 +1,19 @@
+import pymongo;
+db = pymongo.MongoClient()["authentication"];
+images = db["loads"];
+model = db["models"];
+
+from keras.models import model_from_json
+
+
 def trainer(params):
+
+    model = model_from_json(params.architecture);
+
+    model.compile(loss=params.config.loss,
+              optimizer=params.config.optimiser,
+              metrics=params.config.metrics)
+
+
+
     return 'damn';

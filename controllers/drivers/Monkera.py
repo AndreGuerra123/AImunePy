d3141d952238a907296eb19cd92d75425f56bfa4
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-09 23:25:18 +0100

    final commit?

---
+++ controllers/drivers/Monkera.py
@@ -0,0 +1,132 @@
+import io
+import json as js
+import bson as bs
+import pymongo as mongo
+from PIL import Image
+
+import numpy as np
+import pydash as p_
+
+import keras
+from keras.preprocessing.image import Iterator
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+
+
+class MongoGenerator(Iterator):
+    def __init__(self,
+                 image_data_generator,
+                 connection={'host':"localhost",'port':12721,'database': "database", 'collection': "collection"},
+                 query={},
+                 location={'image': "image", 'label': "label"},
+                 config={'batchsize':5,'shuffle':True,'seed':123,'width':50,'height':50}):
+       
+        # Validate inputs
+        assert isinstance(image_data_generator,ImageDataGenerator), "Please provide a valid instance of ImageDataGenerator for data augmentation."
+        self.image_data_generator = image_data_generator
+
+        #Check if inputs are json deserialised objects
+        _ad(connection,"Please select a valid connection dictionary")
+        _ad(query,"Please select a valid query dictionary")
+        _ad(location,"Please select a valid location dictionary")
+        _ad(config,"Please select a valid config dictionary")
+
+        #Check for type an load
+        self._host = _agt(connection,'host',str,"Please provide a valid string for mongodb hostname.")
+        self._port = _agt(connection,'port',int,"Please provide a valid integer for mongodb port.")
+        self._database = _agt(connection,'database',str,"Please provide a valid string for mongodb database.")
+        self._collection = _agt(connection,'collection',str,"Please provide a valid string for mongodb collection.")
+
+        self._img_location = _agt(location,'image',str,"Please provide a valid location for the image binary field in the selected mongodb collection.")
+        self._lbl_location = _agt(location,'image',str,"Please provide a valid location for the label field in the selected mongodb collection.")
+
+        self._batchsize = _agt(config,'batchsize',int,"Please select a valid integer value for the batchsize parameter.")
+        self._shuffle = _agt(config,'shuffle',bool,"Please select a valid boolean value for the shuffle parameter.")
+        self._seed = _agt(config,'seed',int,"Please select a valid integer value for the seed parameter.")
+        self._heigth = _agt(config,'height',int,"Please select a valid integer value for the image height parameter.")
+        self._width = _agt(config,'width',int,"Please select a valid integer value for the image width parameter.")
+
+        self._dtype = K.floatx()
+        self._size = (self._heigth,self._width)
+        self._object_ids = self._getOBIDS(query)
+
+        self._samples = len(self._object_ids)
+        assert (self._samples > 0),"The resulted query returned zero(0) samples."
+        assert (self._samples > self._batchsize),"The resulted query returned less samples than the selected batchsize."
+
+        self._classes = self._getClassNumber()
+        assert (self._classes > 1),"The resulted query return insufficient distinct classes."
+
+        super(MongoGenerator, self).__init__(self._samples, self._batchsize, self._shuffle, self._seed)
+
+    def _getOBIDS(self,query):
+        collection = self._connect()
+        object_ids = collection.find(query, {'_id': True})
+        self._disconnect(collection)
+        return object_ids
+
+    def _getClassNumber(self,query):
+        collection = self._connect()
+        object_ids = collection.find({'_id':{'$in':self._object_ids}}).distinct({self._lbl_location: {'$exists': true}})
+        self._disconnect(collection)
+
+    def _get_batches_of_transformed_samples(self, index_array):
+
+        batch_x = np.zeros((len(index_array),self._height,self._width,3), dtype=self._dtype)
+        
+        batch_y = np.zeros((len(index_array),self._classes), dtype=self._dtype)
+
+        for i, j in enumerate(index_array):
+
+            #Get sample data
+            (x,y) = self.__readMongoSample(self._object_ids[j])
+
+            x = self.image_data_generator.random_transform(x)
+            x = self.image_data_generator.standardize(x)
+
+            # Add the image and the label to the batch (one-hot encoded).
+            batch_x[i] = x
+            batch_y[i] = y
+
+            return batch_x, batch_y
+
+    def next(self):
+        return self._get_batches_of_transformed_samples(next(self.index_generator))
+
+    def _readMongoSample(self, oid):
+
+        collection = self._connect()
+        sample = collection.find({'_id': oid}).next()
+        self._disconnect(collection)
+        return (self._getImage(sample),self._getLabel(sample))
+
+    def _connect(self):
+        return pymongo.MongoClient(self._host,self._port)[self._database][self._collection]
+
+    def _disconnect(self, collection):
+        del collection
+        collection = None
+
+    def _getImage(self,sample):
+        strg = self._getField(sample, self._img_location);
+        img = img.open(io.BytesIO(strg)).resize(self._size)
+        return np.asarray(img, dtype=self._dtype)
+
+    def _geLabel(self,label):
+        label = self._getField(sample, self._lbl_location);
+        return keras.utils.to_categorical(label, self._classes)
+        
+    def _get(obj,loc):
+        return p_.get(obj,loc);
+
+    def _agt(obj,loc,typ,msg):
+        pro = _get(obj,loc)
+        assert pro != None, msg
+        assert type(pro) is typ, msg
+        return pro
+
+    def _ad(object,msg):
+        assert (type(object) is dict),msg
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,81 @@
-from flask import Flask, request, jsonify
+from controllers.drivers.Monkera import MongoIterator
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+# initiate RMSprop optimizer
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+# Let's train the model using RMSprop
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        zca_epsilon=1e-06,  # epsilon for ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        # randomly shift images horizontally (fraction of total width)
+        width_shift_range=0.1,
+        # randomly shift images vertically (fraction of total height)
+        height_shift_range=0.1,
+        shear_range=0.,  # set range for random shear
+        zoom_range=0.,  # set range for random zoom
+        channel_shift_range=0.,  # set range for random channel shifts
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        cval=0.,  # value used for fill_mode = "constant"
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False,  # randomly flip images
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.1)
+
+mongogen = MongoGenerator(datagen,
+                 connection={'host':"localhost",'port':27017,'database': "authentication", 'collection': "loads"},
+                 query={},
+                 location={'image': "image.data", 'label': "classi"},
+                 config={'batchsize':2,'shuffle':True,'seed':123,'width':50,'height':50})
+
+model.fit_generator(datagen, epochs=3, workers=4)
+
+
+
+""" from flask import Flask, request, jsonify
 from flask_cors import CORS
 from controllers.train import trainer
 from controllers.predict import predictor
@@ -45,3 +122,7 @@ def server_error(e):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+ """
+
+
+

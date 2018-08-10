31c4cdae95fdaa70a78768c111e3c30fb5dc6de8
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-10 01:29:27 +0100

    asas

--- controllers/drivers/Monkera.py
+++ controllers/drivers/Monkera.py
@@ -83,11 +83,9 @@ class MongoGenerator(Iterator):
 
     def __getClassNumber(self):
         collection = self.__connect()
-        classes = collection.find({'_id':{'$in':self._object_ids}},self._lbl_location).distinct(self._lbl_location)
-        for i in classes:
-             print(i)
-        self.__disconnect(collection)
-        return classes.length
+        classes = collection.distinct(self._lbl_location,{'_id':{'$in':self._object_ids}}).length
+        print(classes)
+        return classes
 
     def _get_batches_of_transformed_samples(self, index_array):
 

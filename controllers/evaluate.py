ebd9db9d456cfb755b79a2aefb0c5a891f37435b
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-15 20:18:38 +0100

    asasas

--- controllers/evaluate.py
+++
@@ -1,2 +0,0 @@
-def evaluator():
-    return 'damn';
--- main.py
+++ main.py
@@ -1,81 +1,4 @@
-from controllers.drivers.Monkera import MongoImageDataGenerator
-
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-
-mongogen = MongoImageDataGenerator(
-                          connection={'host': "localhost", 'port': 27017,'database': "authentication", 'collection': "loads"},
-                          query={},
-                          location={'image': "image.data", 'label': "classi"},
-                          config={'batch_size': 2, 'shuffle': True, 'seed': 123, 'width': 50, 'height': 50, 'data_format': 'channels_last',
-                            'color_format': 'RGB',
-                            'validation_split': 0.5},
-                          stand={
-                            'preprocessing_function': None,
-                            'rescale': 1/255,
-                            'center': True,
-                            'normalize': True
-                            },
-                          affine={
-                            'rounds': 1,
-                            'transform': True,
-                            'random': False,
-                            'keep_original': False,
-                            'rotation': 0.1,
-                            'width_shift': 0.2,
-                            'height_shift': 0.3,
-                            'shear': 0.1,
-                            'channel_shift': 0.1,
-                            'brightness': 0.4,
-                            'zoom': 0.1,
-                            'horizontal_flip': False,
-                            'vertical_flip': False,
-                            'fill_mode': 'nearest',
-                            'cval': 0.
-                            }
-                          )
-                          
-traingen, valgen = mongogen.flows_from_mongo()
-
-
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same', input_shape = mongogen.getShape()))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(mongogen.getClassNumber()))
-model.add(Activation('softmax'))
-
-# initiate RMSprop optimizer
-opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-
-# Let's train the model using RMSprop
-model.compile(loss='categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
-
-trainflow,testflow = mongogen.flows_from_mongo()
-
-model.fit_generator(traingen, epochs=10,validation_data=valgen, workers=4, use_multiprocessing=True)
-
-
-""" from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from controllers.train import trainer
 from controllers.predict import predictor
@@ -84,29 +7,19 @@ from controllers.evaluate import evaluator
 app = Flask(__name__)
 CORS(app)
 
-# this will route the request to the an API wich performs modelling in tensorflow
-
+# this will route the request to the an API wich performs model training, saves weights and resutls
 
 @app.route("/train", methods=['POST'])
 def train():
     return jsonify(trainer(request.json))
 
-# this will route the request to the an API wich performs metrics evaluation in tensorflow
-
-
-@app.route("/evaluate", methods=['POST'])
-def evaluate():
-    return jsonify(evaluator(request.json))
-
 # this will route the request to the an API wich performs image prediciton in tensorflow
-
 
 @app.route("/predict", methods=['POST'])
 def predict():
     return jsonify(predictor(request.json))
 
 # HTTP Errors handlers
-
 
 @app.errorhandler(404)
 def url_error(e):
@@ -122,4 +35,3 @@ def server_error(e):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
- """

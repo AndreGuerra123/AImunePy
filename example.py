de2283e0bbc87ced9cca0ffdaa9b7f337ba58492
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-17 18:51:06 +0100

    asaass

--- example.py
+++ example.py
@@ -73,7 +73,7 @@ json_model=json.loads(stringmodel)
 json_model['config'][0]['config']['input_shape'] = shape
 json_model['config'][0]['config']['batch_input_shape'] = (None,)+shape
 
-json_model['config'][-1]['config']['input_shape'] = (None,6)
+json_model['config'][-1]['config']['shape'] = (6)
 
 model = model_from_json(json.dumps(json_model))
 

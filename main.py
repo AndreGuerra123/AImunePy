c78549c4fd623ab8c94326067dbbd2043103bb0c
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-21 23:21:16 +0100

    asas

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ CORS(app)
 @app.route("/train", methods=['POST'])
 def train():
     Trainer(request.json)
-    return jsonify("Train completed successfuly.")
+    return jsonify("Train finished.")
     # this will route the request to the an API wich performs image prediciton in tensorflow
 
 @app.route("/predict", methods=['POST'])

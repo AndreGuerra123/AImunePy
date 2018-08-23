b6d1494c421ad9f333d1c9b9c6b4841eaaea834b
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-23 17:48:38 +0100

    asasa

--- read.py
+++ read.py
@@ -4,10 +4,10 @@ from keras.models import model_from_json
 import pydash as p_
 import bson
 import json
-from io import StringIO
+from io import BytesIO
   
 col =  MongoClient('localhost',27017)['authentication']['architectures']
 arch = col.find_one()
 binary = p_.get(arch,'file')
-bina = StringIO(binary)
+bina = BytesIO(binary)
 print(bina)

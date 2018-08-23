36d926c8c99318a6d68a6d15ff5278ce1cdeea89
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-23 17:26:40 +0100

    asass

--- read.py
+++ read.py
@@ -3,9 +3,11 @@ import json
 from keras.models import model_from_json
 import pydash as p_
 import bson
+import io
   
 col =  MongoClient('localhost',27017)['authentication']['architectures']
 arch = col.find_one()
 binary = p_.get(arch,'file')
-model = model_from_json(bson.dumps(binary))
+jsonstr = io.BytesIO(binary)
+model = model_from_json()
 print(model)

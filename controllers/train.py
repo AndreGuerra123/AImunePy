58de1853edecaf10c7fc85040131dda8860d67c2
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-23 11:39:41 +0100

    assas

--- controllers/train.py
+++ controllers/train.py
@@ -157,7 +157,7 @@ class Trainer:
 
     def loadModelArchitecture(self, model_doc):
         arch = getSafe(model_doc,'architecture.file',dict,'Failed to load the model architecture object.')
-        return model_from_json(arch)
+        return model_from_json(json.dumps(arch))
         
 
     def parameterValidation(model):

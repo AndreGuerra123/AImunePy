f3acb835ab85a46782835dc6f9281cba71f7bd3f
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-23 17:15:20 +0100

    asas

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ from keras.models import model_from_json
 import io
   
 col =  MongoClient('localhost',27017)['authentication']['architectures']
-
+print(col.find_one())
 json = json.loads(col.find_one().file)
 
 print(json)

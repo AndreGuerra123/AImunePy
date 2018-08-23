e44fc3ef1437923232c97796bd1c0aefd4eb4089
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-23 18:20:26 +0100

    asas

--- read.py
+++ read.py
@@ -8,6 +8,5 @@ import pydash as p_
   
 col =  MongoClient('localhost',27017)['authentication']['architectures']
 arch = col.find_one()
-binary = p_.get(arch,'file').decode()
-bina = json.loads(binary)
-print(bina)
+decoded = p_.get(arch,'file').decode()
+print(decoded)

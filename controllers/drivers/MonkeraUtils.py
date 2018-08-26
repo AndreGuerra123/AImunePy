69a609f904174d25f09872bc1b8a32be3a4a2dc6
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-26 21:02:05 +0100

    asas

--- controllers/drivers/MonkeraUtils.py
+++ controllers/drivers/MonkeraUtils.py
@@ -110,7 +110,7 @@ def _to_model(archi):
             return model_from_json(base64.b64decode(archi).decode()) # the string was encoded in base64
 
 class MonkeraCallback(Callback):
-    def __init__(query,config={'ini':0,'end':1,'epochs':1,'value':'value','description':'description'},connection={'host':'localhost','port':27017,'database':'database','collection':'collection'}):
+    def __init__(self,query,config={'ini':0,'end':1,'epochs':1,'value':'value','description':'description'},connection={'host':'localhost','port':27017,'database':'database','collection':'collection'}):
          assert type(query) == dict and bool(query), 'Please insert a valid dictionary for the query parameter. Ex:"{"_id":xxx}"'
          self.query = query
          _sprint(_get(config,'ini'))

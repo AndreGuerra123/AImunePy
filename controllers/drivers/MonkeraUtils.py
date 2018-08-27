fcc5e7280b0b87df91329eae4dd829dcbf86a825
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-27 23:52:18 +0100

    aasass

--- controllers/drivers/MonkeraUtils.py
+++ controllers/drivers/MonkeraUtils.py
@@ -215,9 +215,10 @@ def PlotHistory(history,width=300,height=300,tools="pan,wheel_zoom,box_zoom,rese
 
     plots=list()
     for x in unique_metrics:
-        plot = figure(title=x,tools=tools, plot_width=width, plot_height=height)
-        if(_get(history,x)): plot.scatter(Range1d(start=1,end=len(_get(history,x))),_get(history,x))
-        if(_get(history,'val_'+x)): plot.scatter(Range1d(start=1,end=len(_get(history,'val_'+x))),_get(history,'val_'+x))
+        
+        plot = figure(title=x,tools=tools,x_axis_label='Epochs', y_axis_label='Value',plot_width=width, plot_height=height)
+        plot.circle(range(1,len(_get(history,x))+1), _get(history,x), legend=x, color="blue",fill_color="white", size=5)
+        plot.circle(range(1,len(_get(history,'val_'+x))+1), _get(history,'val_'+x), legend='val_'+x, color="green",fill_color="white", size=5)
         plots.append(plot)
 
     return file_html(plots,CDN)

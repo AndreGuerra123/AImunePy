63ce1557702de783dc3e177571bc2a10da21e428
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-29 17:40:39 +0100

    asasas

--- controllers/predict.py
+++ controllers/predict.py
@@ -66,7 +66,7 @@ class Predictor:
         self.model = LoadModelArchitecture({'_id':self.modelid},'architecture.file',MODELS)
         LoadModelWeights(self.model,_getSafe(self.model_doc,'weights',(str,ObjectId),'Could not find a valid weights model file.'),DATABASE)
         self.hotlabels = _getSafe(self.model_doc,'hotlabels',dict,'Could not find a valid hot labels dictionary.')
-
+        print(self.hotlabels)
         self.sample = ValidateImageModel({'_id':self.tempid},'image.data',TEMPS,config={
             'target_size':(_getSafe(self.model_doc,'dataset.height',int,'Failed to retrieve target height integer.'),_getSafe(self.model_doc,'dataset.width',int,'Failed to retrieve target width integer.')),
             'data_format':_getSafe(self.model_doc,'dataset.data_format',str,'Failed to retrieve target data_format string.'),
